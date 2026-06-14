c9e7d55f73d6e5cfce0363988b5a446e1b2f4f50
voruti <45130096+voruti@users.noreply.github.com>
2026-06-14 21:57:01 +0200

    Initialize from Spring Boot Initializer

---
+++ src/main/java/de/redno/aiparticipant/AiparticipantApplication.java
@@ -0,0 +1,13 @@
+package de.redno.aiparticipant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiparticipantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiparticipantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/redno/aiparticipant/AiparticipantApplicationTests.java
@@ -0,0 +1,13 @@
+package de.redno.aiparticipant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiparticipantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
