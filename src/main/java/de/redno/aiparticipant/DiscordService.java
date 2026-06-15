7e237f0e2c70befa66340bd2dda0ad6c00c3f05e
voruti <45130096+voruti@users.noreply.github.com>
2026-06-15 23:30:50 +0200

    Extract conversion and forward username to AI

--- src/main/java/de/redno/aiparticipant/DiscordService.java
+++ src/main/java/de/redno/aiparticipant/DiscordService.java
@@ -1,9 +1,12 @@
 package de.redno.aiparticipant;
 
 import java.util.EnumSet;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Stream;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageHistory;
 import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -40,13 +43,48 @@ public class DiscordService extends ListenerAdapter {
     channel.sendMessage(message).queue();
   }
 
+  private static List<org.springframework.ai.chat.messages.Message> convertToAiMessages(
+      final MessageHistory history, final Message message) {
+    return Stream.<org.springframework.ai.chat.messages.Message>concat(
+            history.getRetrievedHistory().stream()
+                .map(
+                    discordMessage -> {
+                      if (discordMessage.getAuthor().isBot()) {
+                        return new AssistantMessage(discordMessage.getContentDisplay());
+                      } else {
+                        return convertToUserMessage(discordMessage);
+                      }
+                    })
+                .toList()
+                .reversed()
+                .stream(),
+            Stream.of(convertToUserMessage(message)))
+        .toList();
+  }
+
+  private static UserMessage convertToUserMessage(final Message message) {
+    return UserMessage.builder()
+        .text(
+            String.format(
+                "[%s] %s: %s",
+                message.getTimeCreated(),
+                message.getAuthor().getEffectiveName(),
+                message.getContentDisplay()))
+        .metadata(
+            Map.of(
+                "timestamp", message.getTimeCreated(),
+                "guild", message.getGuild().getName(),
+                "channel", message.getChannel().getName(),
+                "author", message.getAuthor().getEffectiveName()))
+        .build();
+  }
+
   @Override
   public void onMessageReceived(final MessageReceivedEvent event) {
     // We don't want to respond to other bot accounts, including ourselves:
     if (event.getAuthor().isBot()) return;
 
     final Message message = event.getMessage();
-    final String content = message.getContentDisplay();
     final MessageChannel channel = event.getChannel();
 
     channel
@@ -55,23 +93,7 @@ public class DiscordService extends ListenerAdapter {
             messages -> {
               DiscordService.channel = channel;
 
-              aiService.handle(
-                  Stream.<org.springframework.ai.chat.messages.Message>concat(
-                          messages.getRetrievedHistory().stream()
-                              .map(
-                                  discordMessage -> {
-                                    if (discordMessage.getAuthor().isBot()) {
-                                      return new AssistantMessage(
-                                          discordMessage.getContentDisplay());
-                                    } else {
-                                      return new UserMessage(discordMessage.getContentDisplay());
-                                    }
-                                  })
-                              .toList()
-                              .reversed()
-                              .stream(),
-                          Stream.of(new UserMessage(content)))
-                      .toList());
+              aiService.handle(DiscordService.convertToAiMessages(messages, message));
             });
   }
 }
