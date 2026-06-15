d9f7d0d95cd0404e6a1845ccbc204eab66206a8a
voruti <45130096+voruti@users.noreply.github.com>
2026-06-15 17:30:22 +0200

    Call service on startup

--- src/main/java/de/redno/aiparticipant/AiparticipantApplication.java
+++ src/main/java/de/redno/aiparticipant/AiparticipantApplication.java
@@ -1,12 +1,21 @@
 package de.redno.aiparticipant;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AiparticipantApplication {
 
-  public static void main(String[] args) {
+  static void main(String[] args) {
     SpringApplication.run(AiparticipantApplication.class, args);
+  }
+
+  @Bean
+  public CommandLineRunner run(ChatController chatController) {
+    return args -> {
+      chatController.logic();
+    };
   }
 }
